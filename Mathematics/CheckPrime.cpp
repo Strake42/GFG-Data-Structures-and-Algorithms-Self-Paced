6ed0901b4a438e73db464ac4ff72101287581a84
Tauqueer Alam <tauqueeralam42@gmail.com>
2023-04-15 15:50:10 +0530

    Maths

--- Mathematics/CheckPrime.cpp
+++ Mathematics/CheckPrime.cpp
@@ -2,20 +2,46 @@
 using namespace std;
 
 //Naive Method
-bool isPrime(int n){
+/*bool isPrime(int n){
     if(n==1)
         return false;
-    for(int i=2; i<n; i++){
+    for(int i=2; i<n; i++)
         if(n%i==0)
          return false;
+    
+
+    return true;
+}*/
+
+//Efficient Method
+/*bool isPrime(int n){
+    if(n==1)
+        return false;
+    for(int i=2; i*i<=n; i++){
+        if(n%i == 0)
+            return false;
     }
+    return true;
+}*/
+
+//More Efficient Method(For large value of n)
+bool isPrime(int n){
+    if(n==1) 
+        return false;
+    if(n==2 || n==3) 
+        return true;
+    if(n%2==0 || n%3==0)
+        return false;
+    for(int i=5; i*i<=n; i=i+6)
+        if(n%i==0 || n%(i+2)==0)
+            return false;
 
     return true;
 }
  
  int main(){
-int number = 17;
-cout<<isPrime(17)<<endl;
+int number = 997;
+cout<<isPrime(number)<<endl;
 
     return 0;
  }
